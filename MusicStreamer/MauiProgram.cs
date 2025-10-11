413a8c8df7299943180ea927e742bb3e440f73e3
Jeffery Ankrom <jefferyankrom@gmail.com>
2025-10-11 18:32:17 -0500

    -Added groundwork for Lifecycle Triggers in Android and iOS

--- MusicStreamer/MauiProgram.cs
+++ MusicStreamer/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace MusicStreamer
 {
@@ -9,6 +10,33 @@ namespace MusicStreamer
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureLifecycleEvents(events =>
+                {
+#if ANDROID
+                    events.AddAndroid(android => android
+                        .OnActivityResult((activity, requestCode, resultCode, data) => LogEvent(nameof(AndroidLifecycle.OnActivityResult), requestCode.ToString()))
+                        .OnStart((activity) => LogEvent(nameof(AndroidLifecycle.OnStart)))
+                        .OnCreate((activity, bundle) => LogEvent(nameof(AndroidLifecycle.OnCreate)))
+                        .OnBackPressed((activity) => LogEvent(nameof(AndroidLifecycle.OnBackPressed)) && false)
+                        .OnStop((activity) => LogEvent(nameof(AndroidLifecycle.OnStop)))
+                        );
+#endif
+#if IOS || MACCATALYST
+                    events.AddiOS(ios => ios
+                        .OnActivated((app) => LogEvent(nameof(iOSLifecycle.OnActivated)))
+                        .OnResignActivation((app) => LogEvent(nameof(iOSLifecycle.OnResignActivation)))
+                        .DidEnterBackground((app) => LogEvent(nameof(iOSLifecycle.DidEnterBackground)))
+                        .WillTerminate((app) => LogEvent(nameof(iOSLifecycle.WillTerminate)))
+                    );
+#endif
+                    static bool LogEvent(string eventName, string type = "Not Provided")
+                    {
+                        //TODO: log to logfile
+                        return true;
+                    }
+                }
+                    )
+                
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -20,6 +48,52 @@ namespace MusicStreamer
 #endif
 
             return builder.Build();
+        }
+
+        public class GlobalDataInfo()
+        {
+            string _username = string.Empty;
+            string _password = string.Empty;
+            string _usertoken = string.Empty;
+
+            public string Username
+            {
+                get
+                { 
+                    return _username; 
+                }
+                private set
+                {
+                    _username = value;
+                }
+            }
+            public string Password
+            {
+                get
+                {
+                    return _password;
+                    //Return hashed password and only unhash before sending to Emby
+                }
+                private set
+                {
+                    _password = value;
+                    //Always hash before sending password
+                }
+            }
+
+            public string Usertoken
+            {
+                get 
+                {
+                    return _usertoken;
+                    //Return hashed usertoken and only unhash before sending to Emby
+                }
+                private set
+                {
+                    _usertoken = value;
+                    //Always hash before sending usertoken
+                }
+            }
         }
     }
 }
